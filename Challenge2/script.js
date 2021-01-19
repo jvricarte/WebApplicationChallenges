34e939d2fb13074aaa3046a45338f130f4405a34
jvricarte <victor_rs11@hotmail.com>
2021-01-19 13:39:16 -0300

    finish update taskDone function

--- Challenge2/script.js
+++ Challenge2/script.js
@@ -43,39 +43,11 @@ function addTask() {
 
 function taskDone(pos) {
     const listOfLi = document.querySelectorAll("li")
-    const findingClass = document.querySelector(".task-done")
-    if (findingClass != undefined) {
-        listOfLi[pos].classList.remove("task-done")
-    } else {
-        listOfLi[pos].classList.add("task-done")
-    }
+    listOfLi[pos].classList.toggle("task-done")
     
-
 }
 
 function taskDelete(pos) {
     taskList.splice(pos, 1)
     showTaskList()
 }
-
-
-
-
-// function addTask() {
-//     let taskInput = document.querySelector('#input').value
-//     let taskOutput = document.createElement("li")
-//     let done = document.createElement("i")
-//     done.classList.add("fas")
-//     done.classList.add("fa-check-square")
-//     let toDelete = document.createElement("i")
-//     toDelete.classList.add("fas")
-//     toDelete.classList.add("fa-trash-alt")
-//     let text = document.createTextNode(taskInput)
-//     taskOutput.appendChild(text)
-//     let breaking = document.createElement("br")
-
-//     document.querySelector("ol").appendChild(done)
-//     document.querySelector("ol").appendChild(toDelete)
-//     document.querySelector("ol").appendChild(taskOutput)
-//     document.querySelector("ol").appendChild(breaking)
-// }
