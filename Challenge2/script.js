92d20763fe58205ad103d7dd67f6e72157e1e764
jvricarte <victor_rs11@hotmail.com>
2021-01-20 11:23:00 -0300

    codigo atualizado para corrigir bug

--- Challenge2/script.js
+++ Challenge2/script.js
@@ -1,15 +1,15 @@
 const ulElement = document.querySelector("ul")
 const input = document.querySelector("#input")
 
-
-const taskList = []
+const taskList = [{
+    taskName: "Teste1", isDone: 0
+}]
 
 function showTaskList() {
     ulElement.innerHTML = ''
-
     for (task of taskList) {
         const liElement = document.createElement("li")
-        const textTask = document.createTextNode(task)
+        const textTask = document.createTextNode(task.taskName)
         const done = document.createElement("i")
         done.classList.add("fas")
         done.classList.add("fa-check-square")
@@ -26,16 +26,22 @@ function showTaskList() {
         liElement.appendChild(textTask)
         ulElement.appendChild(liElement)
         ulElement.appendChild(breaking)
-    }
 
+        if (task.isDone == 1) {
+            liElement.classList.add("task-done")
+        }
+    }
 }
 
 const button = document.querySelector(".fas.fa-plus-square")
 button.addEventListener('click', addTask)
 
 function addTask() {
+    const obj = new Object
     const inputTask = input.value
-    taskList.push(inputTask)
+    obj.taskName = inputTask
+    obj.isDone = 0
+    taskList.push(obj)
     input.value = ''
 
     showTaskList()
@@ -44,7 +50,7 @@ function addTask() {
 function taskDone(pos) {
     const listOfLi = document.querySelectorAll("li")
     listOfLi[pos].classList.toggle("task-done")
-    
+    taskList[pos].isDone == 0 ? taskList[pos].isDone = 1 : taskList[pos].isDone = 0
 }
 
 function taskDelete(pos) {
