6cb405a6426a54b5734b3444a2dab76921825d2f
jvricarte <victor_rs11@hotmail.com>
2021-01-26 00:23:52 -0300

    refactoring to persist data on localstorage/bugs fixed

--- Challenge2/script.js
+++ Challenge2/script.js
@@ -1,22 +1,22 @@
-window.addEventListener('onload', showTaskList)
 const ulElement = document.querySelector("ul")
 const input = document.querySelector("#input")
 
-const taskList = []
+let taskList = []
 
 function showTaskList() {
+    const newArray = getLocalStorage()
+    taskList = JSON.parse(newArray)
     ulElement.innerHTML = ''
     for (task of taskList) {
         const liElement = document.createElement("li")
-        const position = taskList.indexOf(task)
-        const localStorageItem = localStorage.getItem(task.id)
-        const textTask = document.createTextNode(localStorageItem)
+        const textTask = document.createTextNode(task.taskName)
         const done = document.createElement("i")
         done.classList.add("fas")
         done.classList.add("fa-check-square")
         const toDelete = document.createElement("i")
         toDelete.classList.add("fas")
         toDelete.classList.add("fa-trash-alt")
+        const position = taskList.indexOf(task)
         done.setAttribute('onclick', `taskDone(${position})`)
         toDelete.setAttribute('onclick', `taskDelete(${position})`)
         const breaking = document.createElement("br")
@@ -44,10 +44,7 @@ function addTask() {
     obj.isDone = 0
     taskList.push(obj)
     input.value = ''
-
-    const id = Math.random().toString(32).substr(2, 9)
-    obj.id = id
-    localStorage.setItem(obj.id, obj.taskName)
+    setLocalStorage()
     showTaskList()
 }
 
@@ -55,10 +52,29 @@ function taskDone(pos) {
     const listOfLi = document.querySelectorAll("li")
     listOfLi[pos].classList.toggle("task-done")
     taskList[pos].isDone == 0 ? taskList[pos].isDone = 1 : taskList[pos].isDone = 0
+    removeLocalStorage()
+    setLocalStorage()
 }
 
 function taskDelete(pos) {
     localStorage.removeItem(taskList[pos].id)
     taskList.splice(pos, 1)
+    removeLocalStorage()
+    setLocalStorage()
+    getLocalStorage()
     showTaskList()
 }
+
+function setLocalStorage() {
+    localStorage.setItem('json', JSON.stringify(taskList))
+}
+
+function getLocalStorage() {
+    return localStorage.getItem('json')
+}
+
+function removeLocalStorage() {
+    localStorage.removeItem('json')
+}
+
+showTaskList()
